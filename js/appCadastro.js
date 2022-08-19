91c0a08f66876d443c6a41c8bae2d01a2bb9269d
[Ana Salazar] <[nana.brandao.99@gmail.com]>
2022-08-19 14:33:20 -0300

    AttCSS

---
+++ js/appCadastro.js
@@ -0,0 +1,17 @@
+
+//======= Tela de Cadastro
+
+
+
+let clientesCadastrados = []
+
+const btnCadastro = document.querySelector('#btnCadastrar')
+
+
+
+btnCadastro.addEventListener('submit', function () { 
+
+    const iRazaoSocial = document.getElementById('iRazaoSocial').value
+    
+    console.log(iRazaoSocial)
+})
